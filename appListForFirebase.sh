546ca9ec2fcfa8f98a32de3373260ef57a691f27
Erik Kaju <erik.kaju@gmail.com>
2022-11-16 15:12:51 +0200

    split firebase build and deployment scripts. github action will use only build script

--- appListForFirebase.sh
+++
@@ -1,4 +0,0 @@
-echo "Generating /apps directory index page! (Needed for Firebase hosting)"
-cd apps
-node generateAppListForFirebase.js > index.html
-cd ..
--- deployToFirebase.sh
+++ deployToFirebase.sh
@@ -1,36 +1,6 @@
 #!/bin/bash
 
-cd apps
-echo "generating /apps directory index file for Firebase"
-cat > dirIndexForFirebase.js << 'EOF'
-
-const { resolve } = require('path'), { readdir } = require('fs').promises;
-
-async function getFiles(dir) {
-    const dirents = await readdir(dir, { withFileTypes: true });
-    const files = await Promise.all(dirents.map((dirent) => {
-            return dirent.name;
-    }));
-    return Array.prototype.concat(...files);
-}
-
-const directory = ".";
-
-getFiles(directory).then(results => {
-    let html = "<ul>"
-        + results.map(fileOrDirectory => {
-            if (fileOrDirectory.includes('.kajuapp')) {
-                return `<li><a href="${fileOrDirectory}">${fileOrDirectory}</a></li>`
-            }
-        }).join("");
-    html += '</ul>';
-    process.stdout.write(html);
-});
-
-EOF
-
-node dirIndexForFirebase.js > index.html
-cd ..
+sh buildForFirebase.sh
 firebase deploy
 rm apps/dirIndexForFirebase.js
 rm apps/index.html
