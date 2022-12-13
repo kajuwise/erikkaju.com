812450236bf6ba0751e8f8012c17505bc1e7db73
Erik Kaju <erik.kaju@gmail.com>
2022-12-13 13:43:56 +0200

    update buildForFirebase.sh script to support apps packaged as folder

--- buildForFirebase.sh
+++ buildForFirebase.sh
@@ -5,23 +5,30 @@ echo "generating /apps directory index file for Firebase"
 cat > dirIndexForFirebase.js << 'EOF'
 
 const { resolve } = require('path'), { readdir } = require('fs').promises;
+const directory = ".";
 
 async function getFiles(dir) {
     const dirents = await readdir(dir, { withFileTypes: true });
-    const files = await Promise.all(dirents.map((dirent) => {
-            return dirent.name;
+    const files = await Promise.all(dirents
+        .filter((dirent) => dirent.name.includes('.kajuapp'))
+        .map((dirent) => {
+            if (dirent.isDirectory()) {
+                return getFiles(resolve(dirent.name));
+            }
+            let direntName = dirent.name;
+            if (dir != directory) {
+                direntName = direntName + "/" + direntName;
+            }
+            return direntName;
     }));
     return Array.prototype.concat(...files);
 }
 
-const directory = ".";
 
 getFiles(directory).then(results => {
     let html = "<ul>"
         + results.map(fileOrDirectory => {
-            if (fileOrDirectory.includes('.kajuapp')) {
-                return `<li><a href="${fileOrDirectory}">${fileOrDirectory}</a></li>`
-            }
+            return `<li><a href="${fileOrDirectory}">${fileOrDirectory}</a></li>`
         }).join("");
     html += '</ul>';
     process.stdout.write(html);
